08f13042ac4a2439f0df086fc7c13d69fc1d8e6b
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-02-01 18:58:42 +0900

    Changes for 0.0.12

--- installer.js
+++ installer.js
@@ -4,46 +4,35 @@ const path = require("path")
 
 const CANONICAL_BINARY = "go-npm-test"
 
-const supported: Record<string, string> = {
+const supported = {
 	"darwin arm64 LE": "darwin-64", // Forward to darwin-64
 	"darwin x64 LE": "darwin-64",
 	"linux x64 LE": "linux-64",
 	"win32 x64 LE": "windows-64.exe",
 }
 
-function bin(name: string) {
+// Resolves to an absolute binary path.
+function bin(name) {
 	return path.join(__dirname, "bin", name)
 }
 
-function createCanonicalBinary(binary: string) {
-	console.log("createCanonicalBinary")
-
+function createCanonicalBinary(binary) {
 	const src = bin(binary)
 	const dst = bin(CANONICAL_BINARY)
-
 	fs.copyFileSync(src, dst)
-	// fs.chmodSync(dst, 0o755) // TODO: Remove?
-
-	fs.rmSync(bin("darwin-64"))
-	fs.rmSync(bin("linux-64"))
-	fs.rmSync(bin("windows-64.exe"))
+	fs.chmodSync(dst, 0o755)
 }
 
 function run() {
 	const platformKey = `${process.platform} ${os.arch()} ${os.endianness()}`
-
 	const binary = supported[platformKey]
 	if (!binary) {
-		console.error(`unsupported platform: ${platformKey}`)
-		process.exit(1)
+		throw new Error(`unsupported platform: ${platformKey}`)
 	}
-
-	// prettier-ignore
 	try {
 		createCanonicalBinary(binary)
 	} catch (err) {
-		throw new Error("an unexpected error occurred: " +
-      ((err && err.message) || err))
+		throw new Error("an unexpected error occurred: " + err.message)
 	}
 }
 
